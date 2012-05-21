b4a7391cb4c795e74090b80898595308509b831c
Alexandr Ruchkin <voidex@live.com>
2012-05-22 02:44:53 +0400

    Lazy patterns

--- src/Data/Serialization/Deserialize.hs
+++ src/Data/Serialization/Deserialize.hs
@@ -10,12 +10,12 @@ newtype Deserialize m a = Deserialize {
     runDeserialize :: m a }
 
 instance (Applicative m, Alternative m, Monad m) => Combine (Deserialize m) where
-    (Deserialize l) .**. (Deserialize r) = Deserialize $ (,) <$> l <*> r
-    (Deserialize l) .++. (Deserialize r) = Deserialize $ (Left <$> l) <|> (Right <$> r)
-    (Deserialize l) .+. (Deserialize r) = Deserialize $ l <|> r
-    (Iso ab ba) <<>> (Deserialize v) = Deserialize $ fmap ab v
-    (Deserialize v) .:. (Iso ab ba) = Deserialize $ fmap ba v
-    (Deserialize l) .*>> f = Deserialize $ do
+    ~(Deserialize l) .**. ~(Deserialize r) = Deserialize $ (,) <$> l <*> r
+    ~(Deserialize l) .++. ~(Deserialize r) = Deserialize $ (Left <$> l) <|> (Right <$> r)
+    ~(Deserialize l) .+. ~(Deserialize r) = Deserialize $ l <|> r
+    ~(Iso ab ba) <<>> ~(Deserialize v) = Deserialize $ fmap ab v
+    ~(Deserialize v) .:. ~(Iso ab ba) = Deserialize $ fmap ba v
+    ~(Deserialize l) .*>> f = Deserialize $ do
         x <- l
         y <- runDeserialize (f x)
         return (x, y)
--- src/Data/Serialization/Serialize.hs
+++ src/Data/Serialization/Serialize.hs
@@ -10,13 +10,13 @@ newtype Serialize m a = Serialize {
     runSerialize :: a -> m () }
 
 instance (Monad m, Applicative m, Alternative m) => Combine (Serialize m) where
-    (Serialize l) .**. (Serialize r) = Serialize $ \(x, y) -> l x >> r y
-    (Serialize l) .++. (Serialize r) = Serialize f where
+    ~(Serialize l) .**. ~(Serialize r) = Serialize $ \(x, y) -> l x >> r y
+    ~(Serialize l) .++. ~(Serialize r) = Serialize f where
         f (Left x) = l x
         f (Right y) = r y
-    (Serialize l) .+. (Serialize r) = Serialize $ \x -> l x <|> r x
-    (Iso ab ba) <<>> (Serialize v) = Serialize $ v . ba
-    (Serialize v) .:. (Iso ab ba) = Serialize $ v . ab
-    (Serialize l) .*>> f = Serialize $ \(x, y) -> l x >> runSerialize (f x) y
+    ~(Serialize l) .+. ~(Serialize r) = Serialize $ \x -> l x <|> r x
+    ~(Iso ab ba) <<>> ~(Serialize v) = Serialize $ v . ba
+    ~(Serialize v) .:. ~(Iso ab ba) = Serialize $ v . ab
+    ~(Serialize l) .*>> f = Serialize $ \(x, y) -> l x >> runSerialize (f x) y
     pures v = Serialize $ \x -> return ()
     fails = Serialize (const empty)
