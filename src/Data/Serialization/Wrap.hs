2f4a408780ded37916d6fd57a11b20860afd253b
Alexandr Ruchkin <voidex@live.com>
2012-11-07 10:43:44 +0400

    Revert "Added dependency"
    This reverts commit 5ab9dd0ab8080993c914fdc8c33bc275bce8ba04.

--- src/Data/Serialization/Wrap.hs
+++ src/Data/Serialization/Wrap.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, DefaultSignatures, TypeFamilies, FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, DefaultSignatures, TypeFamilies #-}
 
 -- | Module provides helpers to implement serializer as simple as possible
 --
@@ -56,7 +56,7 @@ import Data.Serialization.Combine
 import Data.Serialization.Generic
 
 -- | Derive to support serialization for @Encoding@.
-class (Monad sm, Applicative sm, Alternative sm) => Serializer sm s | sm -> s where
+class (Monad sm, Applicative sm, Alternative sm) => Serializer sm s where
     serialize :: sm () -> Either String s
     serializeTail :: s -> sm ()
 
@@ -66,7 +66,7 @@ class (Monad sm, Applicative sm, Alternative sm) => Serializer sm s | sm -> s wh
     serializeTail = tell
 
 -- | Derive to support deserialization for @Decoding@
-class (Monad dm, Applicative dm, Alternative dm) => Deserializer dm s | dm -> s where
+class (Monad dm, Applicative dm, Alternative dm) => Deserializer dm s where
     deserialize :: dm a -> s -> Either String a
     deserializeEof :: Hint s -> dm ()
     deserializeTail :: dm s
